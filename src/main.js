0adb211eec98039b79c37a7dcee9753ad6f5175e
Enzo Cranford <enzo@cranford.fr>
2019-05-03 12:14:27 +0200

    Play sound on circle clicked

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 function init () {
   let stage = new createjs.Stage('canvas');
   let circle = new createjs.Shape();
+
   circle.graphics.beginFill('DeepSkyBlue').drawCircle(0, 0, 50);
   circle.x = 100;
   circle.y = 100;
+
   stage.addChild(circle);
   stage.update();
+
+  circle.addEventListener('click', circleClick);
+}
+
+let sound = new Howl({
+  src: ['assets/sound.wav']
+});
+
+function circleClick (event) {
+  sound.play();
 }
