45962335fa3ee32cbf1b18636f5f7ca9a4feefb8
Enzo Cranford <enzo@cranford.fr>
2019-05-20 11:30:03 +0200

    Add play, pause and stop tests and functionality

--- src/instrument.js
+++ src/instrument.js
@@ -1,9 +1,21 @@
 export default class Instrument {
+  constructor () {
+    this.state = 'stopped';
+  }
+  play () {
+    this.state = 'playing';
+    this.playSound();
+  }
+  pause () {
+    this.state = 'paused';
+    this.pauseSound();
+  }
+  stop () {
+    this.state = 'stopped';
+    this.stopSound();
+  }
   shape (color, x, y, radius) {
     return this.createShape(color, x, y, radius);
-  }
-  sound (path) {
-    return this.createSound(path);
   }
   createShape (color, x, y, radius) {
     const shape = new window.createjs.Shape();
@@ -11,7 +23,19 @@ export default class Instrument {
     shape.graphics.beginFill(color).drawCircle(x, y, radius);
     return shape;
   }
+  initSound (path) {
+    this.createSound(path);
+  }
+  playSound () {
+    this.sound.play();
+  }
+  pauseSound () {
+    this.sound.pause();
+  }
+  stopSound () {
+    this.sound.stop();
+  }
   createSound (path) {
-    return new Howl({ src: [path], loop: true });
+    this.sound = new Howl({ src: [path], loop: true });
   }
 }
