068f61d0c91e8f95e98dbe9ed2548e71b2040cd4
Enzo Cranford <enzo@cranford.fr>
2019-05-22 17:44:57 +0200

    Add mute tests

--- tests/test_instrument.js
+++ tests/test_instrument.js
@@ -2,7 +2,7 @@ import jsdom from 'jsdom-global';
 import Instrument from '../src/instrument';
 
 describe('Instrument', () => {
-  it('Creates an easlejs Shape', () => {
+  it('.initElement creates a div element', () => {
     let called = false;
     const instrument = new Instrument();
     instrument.createElement = () => { called = true; };
@@ -10,7 +10,7 @@ describe('Instrument', () => {
     instrument.initElement('#0d72c4');
     expect(called).to.be(true);
   });
-  it('Create a new Howl', () => {
+  it('.initSound creates a new Howl', () => {
     let called = false;
     const instrument = new Instrument();
     instrument.createSound = () => { called = true; };
@@ -18,25 +18,32 @@ describe('Instrument', () => {
     instrument.initSound('assets/sound.wav');
     expect(called).to.be(true);
   });
-  it('Instrument.play plays instrument.sound', () => {
+  it('.play plays instrument.sound', () => {
     const instrument = new Instrument();
     instrument.playSound = () => {};
 
     instrument.play();
     expect(instrument.state).to.equal('playing');
   });
-  it('Instrument.pause stops instrument.sound', () => {
+  it('.pause stops instrument.sound', () => {
     const instrument = new Instrument();
     instrument.pauseSound = () => {};
 
     instrument.pause();
     expect(instrument.state).to.equal('paused');
   });
-  it('Instrument.stop stops instrument.sound', () => {
+  it('.stop stops instrument.sound', () => {
     const instrument = new Instrument();
     instrument.stopSound = () => {};
 
     instrument.stop();
     expect(instrument.state).to.equal('stopped');
+  });
+  it('.mute mutes instrument.sound', () => {
+    const instrument = new Instrument();
+    instrument.toggleSound = () => {};
+
+    instrument.toggleMute();
+    expect(instrument.state).to.equal('muted');
   });
 });
