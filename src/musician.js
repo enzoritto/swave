f402d4091898acbcdd39f80de63d7e3b8db6fc33
Enzo Cranford <enzo@cranford.fr>
2019-05-31 15:08:53 +0200

    Minor changes
    -Moved const variables to init function.
    -Made the musician in charge of displaying themselves.

--- src/musician.js
+++ src/musician.js
@@ -3,51 +3,60 @@ export default class Musician {
     this.state = 'stopped';
     this.muted = false;
     this.initElement(color);
-    this.initSound(soundPath);
+    this.sound = new Howl({ src: [soundPath + '.ogg', soundPath + '.mp3'], loop: true });
     this.element.addEventListener('click', () => { this.toggleMute(); });
   }
+
   play () {
     this.state = 'playing';
     this.playSound();
   }
+
   pause () {
     this.state = 'paused';
     this.pauseSound();
   }
+
   stop () {
     this.state = 'stopped';
     this.stopSound();
   }
+
   toggleMute () {
     if(!this.muted) { this.state = 'muted'; }
     else { this.state = 'playing'; }
     this.toggleSound();
   }
+
   initElement (color) {
     this.createElement(color);
   }
+
   createElement (color) {
     this.element = document.createElement('div');
     this.element.className = 'musician';
     this.element.style.backgroundColor = color;
   }
-  initSound (path) {
-    this.createSound(path);
+
+  display (parent) {
+    parent.appendChild(this.element);
   }
-  createSound (path) {
-    this.sound = new Howl({ src: [path + '.ogg', path + '.mp3'], loop: true });
-  }
+
   playSound () {
     this.sound.play();
   }
+
   pauseSound () {
     this.sound.pause();
   }
+
   stopSound () {
     this.sound.stop();
   }
+
   toggleSound () {
     this.muted = !this.muted;
     this.sound.mute(!this.sound.mute());
   }
+
 }
