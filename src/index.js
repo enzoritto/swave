6fba8ee1c3cf254b680f9b19d4406a772b89104c
Enzo Cranford <enzo@cranford.fr>
2019-06-04 15:06:33 +0200

    Add adding musicians
    -Installed and setup tippy.js
    -Added add musician button (stage.js)
    -Added guitar loops

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import './style.scss';
-import './assets/piano.ogg';
-import './assets/piano.mp3';
-import './assets/drums.ogg';
-import './assets/drums.mp3';
 import Musician from './musician';
 import Stage from './stage';
 
 function init () {
-  const musicians = [];
-  const stage = new Stage(musicians);
   const musiciansElement = document.getElementById('musicians');
+  const stage = new Stage(musiciansElement);
 
-  stage.initStage(musiciansElement);
+  stage.initStage();
   stage.initToolbar();
-
-  musicians.forEach((musician) => {
-    musician.display(musiciansElement);
-  });
 }
 
 window.onload = init;
