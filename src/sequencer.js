4d479af1de198fc77e270898c570b44fd91f03b4
zoford <enzo@cranford.fr>
2019-07-30 15:46:46 +0200

    Change instruments

--- src/sequencer.js
+++ src/sequencer.js
@@ -39,15 +39,19 @@ export default class Sequencer {
         if (note !== null) this.instrument.triggerAttackRelease(note, '8n');
       }, new Array(16)).start());
       this.sequences[i].loop = true;
+      this.sequences[i].loopEnd = '4m';
+      this.sequences[i].loopStart = 0;
+      this.sequences[i].humanize = true;
       console.log(this.sequences[i].length);
       Array.prototype.forEach.call(row.children, (e, j) => {
         if (e.classList.contains('note')) {
           e.addEventListener('click', () => {
             if (e.classList.contains('off')) {
-              this.sequences[i].add(j, this.notes[i]);
+              this.sequences[i].add(j - 1, this.notes[i]);
+              this.instrument.triggerAttackRelease(this.notes[i], '8n');
               e.classList.replace('off', 'on');
             } else {
-              this.sequences[i].remove(j);
+              this.sequences[i].remove(j - 1);
               e.classList.replace('on', 'off');
             }
           });
